c9742abc72625ac9d9d8ec1e45167c45a9687991
Andrew Matz <andy@jamdesignco.com>
2025-06-01 17:35:43 -0500

    Create a blank multi-platform swift application

---
+++ learn.Swift/learn.Swift/learn_SwiftApp.swift
@@ -0,0 +1,32 @@
+//
+//  learn_SwiftApp.swift
+//  learn.Swift
+//
+//  Created by Andrew Matz on 6/1/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct learn_SwiftApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ learn.Swift/learn.SwiftTests/learn_SwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  learn_SwiftTests.swift
+//  learn.SwiftTests
+//
+//  Created by Andrew Matz on 6/1/25.
+//
+
+import Testing
+
+struct learn_SwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
